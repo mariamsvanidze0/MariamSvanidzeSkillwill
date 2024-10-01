12d6ba975e8eb5d93a4a219b3fd286af142ba3d2
mariamsvanidze0 <mariamsvanidze00@gmail.com>
2024-10-01 19:53:08 +0300

    Finished working on workshop31

---
+++ workshop31/beer-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BeerList from './components/BeerList';
+
+const App = () => {
+  return (
+    <div>
+      <BeerList />
+    </div>
+  );
+};
+
+export default App;
